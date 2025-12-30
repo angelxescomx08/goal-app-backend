361223912d9bf1ce206a537df2e9b5e8c3d9480f
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-12-29 21:21:39 -0600

    env, drizzle and docker for databse

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,9 +7,9 @@ export const auth = betterAuth({
     enabled: true,
   },
   socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
   database: drizzleAdapter(db, {
