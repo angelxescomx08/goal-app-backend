0972c19c73ea1e7f3ae8723ef67adbf51489c139
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-12-30 20:47:23 -0600

    connection to log in

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Elysia, Context } from "elysia";
 import { auth } from "./lib/auth";
+import { cors } from '@elysiajs/cors'
 
 const betterAuthView = (context: Context) => {
   const BETTER_AUTH_ACCEPT_METHODS = ["POST", "GET"]
@@ -11,7 +12,9 @@ const betterAuthView = (context: Context) => {
   }
 }
 
-const app = new Elysia().all("/api/auth/*", betterAuthView).listen(3000);
+const app = new Elysia().use(cors({
+  origin: ['http://localhost:1420'],
+})).all("/api/auth/*", betterAuthView).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,12 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/db";
+import * as schema from "../db/schema";
 
 export const auth = betterAuth({
+  trustedOrigins: [
+    'http://localhost:1420'
+  ],
   emailAndPassword: {
     enabled: true,
   },
@@ -14,5 +18,6 @@ export const auth = betterAuth({
   },
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
 });
