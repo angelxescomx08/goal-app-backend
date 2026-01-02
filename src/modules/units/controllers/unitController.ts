1e41054c42e8b9438cd721c625749fa316117e6d
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2026-01-01 21:19:32 -0600

    create unit

--- src/modules/units/controllers/unitController.ts
+++ src/modules/units/controllers/unitController.ts
@@ -1,11 +1,32 @@
 import { db } from "../../../db/db";
 import { units } from "../../../db/schema";
-import { eq } from "drizzle-orm";
 import { Session } from "../../../lib/auth";
+import { CreateUnitSchema } from "../schemas/unitSchema";
+import { Context } from "elysia";
+import crypto from "node:crypto";
 
-export async function getUnitsByUser(session: Session) {
-  const userUnits = await db.select().from(units).where(eq(units.id, session.session.userId));
+export async function getUnitsByUser() {
+  const userUnits = await db.select().from(units);
   return {
     units: userUnits,
+  }
+}
+
+export async function createUnit(context: {
+  body: CreateUnitSchema,
+  session: Session["session"],
+  user: Session["user"],
+  status: Context["status"]
+}) {
+  const { body, status } = context;
+  try {
+    const newUnit = await db.insert(units).values({
+      id: crypto.randomUUID(),
+      name: body.name,
+    }).returning();
+    return status(201, { unit: newUnit });
+  } catch (error) {
+    console.error(error);
+    return status(500, { error: "Falló la creación de la unidad" });
   }
 }
--- src/modules/units/routes/unitRouter.ts
+++ src/modules/units/routes/unitRouter.ts
@@ -1,9 +1,15 @@
-import { getUnitsByUser } from "../controllers/unitController";
+import { createUnit, getUnitsByUser } from "../controllers/unitController";
 import { betterAuthMiddleware } from "../../../lib/auth";
+import { createUnitSchema } from "../schemas/unitSchema";
 
 export const unitRouter = betterAuthMiddleware
   .group("/units", (group) =>
-    group.get("/by-user", getUnitsByUser, {
-      auth: true,
-    })
+    group
+      .get("/by-user", getUnitsByUser, {
+        auth: true,
+      })
+      .post("/", createUnit, {
+        auth: true,
+        body: createUnitSchema,
+      })
   );
